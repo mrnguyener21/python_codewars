7f296aec6529975779b4a68ccccec9c95296f13b
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-01-12 18:11:35 -0800

    compleeted sum of positive

--- python_codewars.py
+++ python_codewars.py
@@ -1,7 +1,22 @@
 # EVEN OR ODD
 #Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-def even_or_odd(number):
-    if number%2 == 0:
-        return'Even'
-    else:
-        return 'Odd'
+# def even_or_odd(number):
+#     if number%2 == 0:
+#         return'Even'
+#     else:
+#         return 'Odd'
+
+
+
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+def positive_sum(arr):
+    sum = 0
+    for x in arr:
+        if x > 0:
+            sum = sum +x
+    return sum
+print(positive_sum([1,2,3]))
