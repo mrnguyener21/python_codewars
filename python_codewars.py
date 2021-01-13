e03de4d9318dd15b98d21a0c9a8dcb4b31736d34
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-01-12 18:16:39 -0800

    completed opposite number

--- python_codewars.py
+++ python_codewars.py
@@ -13,10 +13,24 @@
 # Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 # Note: if there is nothing to sum, the sum is default to 0.
-def positive_sum(arr):
-    sum = 0
-    for x in arr:
-        if x > 0:
-            sum = sum +x
-    return sum
-print(positive_sum([1,2,3]))
+# def positive_sum(arr):
+#     sum = 0
+#     for x in arr:
+#         if x > 0:
+#             sum = sum +x
+#     return sum
+# print(positive_sum([1,2,3]))
+
+
+#opposite number
+# Very simple, given a number, find its opposite.
+
+# Examples:
+
+# 1: -1
+# 14: -14
+# -34: 34\
+def opposite(number):
+    return number * -1
+
+print( opposite(26.5))
