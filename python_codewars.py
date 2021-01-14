0d6b759a3c2ee2e6913dc9e777fbd63be5fd4eac
Victor Nguyen <vnguyen@triconah.com>
2021-01-14 12:32:26 -0800

    solved sum of positie

--- python_codewars.py
+++ python_codewars.py
@@ -30,7 +30,23 @@
 # 1: -1
 # 14: -14
 # -34: 34\
-def opposite(number):
-    return number * -1
+# def opposite(number):
+#     return number * -1
 
-print( opposite(26.5))
+# print( opposite(26.5))
+
+
+#sum of positive
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+def postive_sum(arr):
+    sum = 0;
+    for x in arr:
+        if x > 0:
+            sum = sum + x;
+    return sum;
+
+print(postive_sum([1,2,3,4,5]))
