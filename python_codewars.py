f7c6bcf20c805ecb9e867503c378f1b89bf1a9f6
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-01-16 12:23:15 -0800

    completed get the middle character

--- python_codewars.py
+++ python_codewars.py
@@ -218,18 +218,58 @@
 # high_and_low("1 9 3 4 -5") # return "9 -5"
 
 #im gonna need to remove the spaces in between
-def high_and_low(numbers):
-    # below is my code
-    # x = numbers.split( )
-    # z = []
-    # for y in x:
-    #     z.append(int(y))
-    # z = sorted(z)
-    # return str(z[len(z)-1]) + ' '+ str(z[0]);
+# def high_and_low(numbers):
+#     # below is my code
+#     # x = numbers.split( )
+#     # z = []
+#     # for y in x:
+#     #     z.append(int(y))
+#     # z = sorted(z)
+#     # return str(z[len(z)-1]) + ' '+ str(z[0]);
 
-    #below is best practice
-    nn = [int(s) for s in numbers.split(" ")]
-    return "{} {}".format(max(nn),min(nn))
+#     #below is best practice
+#     nn = [int(s) for s in numbers.split(" ")]
+#     return "{} {}".format(max(nn),min(nn))
 
 # high_and_low('12 2 3 4 5')
 # print(high_and_low('1 2 -3 4 5'))
+
+
+#Get the middle character
+# You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+# #Examples:
+
+# Kata.getMiddle("test") should return "es"
+
+# Kata.getMiddle("testing") should return "t"
+
+# Kata.getMiddle("middle") should return "dd"
+
+# Kata.getMiddle("A") should return "A"
+# #Input
+
+# import math
+# def get_middle(s):
+    #below is my code
+    # x = list(s)
+    # y = round(len(x)/2 - 0.5)
+    # z = round(len(x)/2)
+
+    # if len(x) %2 != 0:
+    #     return x[y]
+    # else:
+    #     return s[z-1:z+1]
+
+    # below is best practice
+    #return s[(len(s)-1)/2:len(s)/2+1]
+
+    #below is my prefered way of doing it 
+    # x = len(s)
+    # y = int(x/2)
+    # if x%2==0:
+    #     return s[y-1:y+1]
+    # else:
+    #     return s[y:y+1]
+
+# print(get_middle('abcdef'))
