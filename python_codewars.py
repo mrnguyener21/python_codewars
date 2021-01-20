3caa4eb704c69baaa4c1a9d75bd68da1d5ccabd2
Victor Nguyen <vnguyen@triconah.com>
2021-01-20 11:40:56 -0800

    completed square every digit

--- python_codewars.py
+++ python_codewars.py
@@ -273,3 +273,18 @@
     #     return s[y:y+1]
 
 # print(get_middle('abcdef'))
+
+
+#square every digit
+#square every digit of  a number and concatenate them
+# For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+# def square_digits(num):
+#     x = list(str(num))
+#     y = ''
+
+#     for number in x:
+#         y += str(int(number) ** 2);
+#     return int(y)
+    
+# print(square_digits(9119))
+
