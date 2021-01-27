ce04dda573b28e2bcd4016f8e7f72f94a84beb1c
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-01-27 09:08:39 -0800

    solved python if-else

--- hackerrank.py
+++ hackerrank.py
@@ -123,3 +123,22 @@
 
 # minion_game('BANANA')
 # print(minion_game('BANANA'))
+
+
+# given an integer, n, perform the following conditional actions:
+#     if n is odd, print 'Weird'
+#     if n is even and in the inclusive range of 2 to 5, print 'Not Weird'
+#     if n is even and in the inclusive range of 6 to 20, print 'Weird'
+#     if n is even and greater than 20, print ' not weird'
+
+    if n%2 != 0:
+        print('Weird')
+    else:
+        if n >= 2 and n <= 5:
+            print('Not Weird')
+        elif n >= 6 and n <= 20:
+            print('Weird')
+        elif n >20:
+            print('Not Weird')
+
+
